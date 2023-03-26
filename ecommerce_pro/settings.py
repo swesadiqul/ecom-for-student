f71bc2117b24c1a35ab02ec4e7f89d535b3b8f9b
swesadiqul <mdsadiqulislam390@gmail.com>
2023-03-27 01:01:09 +0600

    code updated

--- ecommerce_pro/settings.py
+++ ecommerce_pro/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mjm^9qw5&@zpr^4c0p+xhaqmp&dqko%^bybk%)8rs6j(z@@=&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
